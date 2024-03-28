a4b47796d8890bd3a041d07680ef85af93f4599a
Colin Huber <cbhuber@gmail.com>
2024-03-28 13:25:44 -0600

    doc: Build file documentation

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+/// Build function for compiling protocol buffers.
+///
+/// This function is used to compile the protocol buffers defined in the `authentication.proto` file.
+///
+/// # Returns
+///
+/// An `Ok(())` result if the protocol buffers are compiled successfully, otherwise an error message.
+///
+/// # Errors
+///
+/// This function returns an error if there are issues with compiling the protocol buffers.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/authentication.proto")?;
     Ok(())
